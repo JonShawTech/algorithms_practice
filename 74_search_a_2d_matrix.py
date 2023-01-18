37b16a7d057bd457ee76ddc7199020195b4961fe
jonshawtech <jonshawtech@gmail.com>
2023-01-18 15:18:22 -0500

    binary search

--- 74_search_a_2d_matrix.py
+++ 74_search_a_2d_matrix.py
@@ -1,4 +1,19 @@
+"""
 
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+"""
+
+# my solution runtime O(mn), m = num columns, n = num rows
+# this could be potentially be done in O(log n * log m) time if matrix is always sorted (with 2 runs of binary search)
 
 class Solution(object):
     def searchMatrix(self, matrix, target):
@@ -7,9 +22,6 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        # matrix = [[1,3,5,7],
-        #          [10,11,16,20],
-        #          [23,30,34,60]]
 
         for row in matrix:
             for element in row:
