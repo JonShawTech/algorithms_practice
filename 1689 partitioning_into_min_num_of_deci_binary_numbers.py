22749a8b0d711e10aa7b1e00765861a8b7872b20
jonshawtech <jonshawtech@gmail.com>
2023-02-08 12:25:35 -0500

    arrays

---
+++ 1689 partitioning_into_min_num_of_deci_binary_numbers.py
@@ -0,0 +1,32 @@
+"""
+A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros.
+For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+
+Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+
+Example 1:
+
+Input: n = "32"
+Output: 3
+Explanation: 10 + 11 + 11 = 32
+
+Example 2:
+
+Input: n = "82734"
+Output: 8
+
+"""
+
+# Time complexity analysis - O(n)
+
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+        max = 0
+        for i in range(len(n)):
+            if int(n[i]) > max:
+                max = int(n[i])
+        return max
